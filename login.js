8610c2b57c2b58a6c589a450e99ecf985076128e
Aarya <93391700+Jeet-Pattani@users.noreply.github.com>
2023-07-17 13:25:02 +0530

    Update login.js

--- login.js
+++ login.js
@@ -8,61 +8,64 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// Prompt the user for input
-rl.question('Enter TOTP: ', (totp) => {
-  const tempOTP = totp
-  
-const clientCode='A123456' //angel client code
-const password = '1234'//4 digit pin
-const apiKey = 'abcDEFh'
+// Prompt the user for the password
+rl.question('Enter password: ', (password) => {
+  // Prompt the user for the TOTP
+  rl.question('Enter TOTP: ', async (tempOTP) => {
+    const clientCode = 'Your Angel Client Code';
+    const apiKey = 'Your SmartAPI key';
 
-const data = JSON.stringify({
-  clientcode: clientCode,
-  password: password,
-  totp: tempOTP,
-});
+    const data = {
+      clientcode: clientCode,
+      password: password,
+      totp: tempOTP
+    };
 
-const config = {
-  method: 'post',
-  url: 'https://apiconnect.angelbroking.com/rest/auth/angelbroking/user/v1/loginByPassword',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'X-UserType': 'USER',
-    'X-SourceID': 'WEB',
-    'X-ClientLocalIP': 'CLIENT_LOCAL_IP',
-    'X-ClientPublicIP': 'CLIENT_PUBLIC_IP',
-    'X-MACAddress': 'MAC_ADDRESS',
-    'X-PrivateKey': apiKey,
-  },
-  data: data,
-};
+    try {
+      const response = await axios.post(
+        'https://apiconnect.angelbroking.com/rest/auth/angelbroking/user/v1/loginByPassword',
+        data,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-UserType': 'USER',
+            'X-SourceID': 'WEB',
+            'X-ClientLocalIP': 'CLIENT_LOCAL_IP',
+            'X-ClientPublicIP': 'CLIENT_PUBLIC_IP',
+            'X-MACAddress': 'MAC_ADDRESS',
+            'X-PrivateKey': apiKey
+          }
+        }
+      );
 
-axios(config)
-  .then(function (response) {
-    console.log(response)
-    const jwtToken = response.data.data.jwtToken;
-    const refreshToken = response.data.data.refreshToken;
-    const feedToken = response.data.data.feedToken;
-    const configData = `
-      module.exports = {
-        jwtToken: '${jwtToken}',
-        refreshToken: '${refreshToken}',
-        feedToken: '${feedToken}',
-        apiKey: '${apiKey}',
-        clientCode:'${clientCode}',
-      };
-    `;
+      if (response.data.message === 'SUCCESS') {
+        console.log('Login Successful...!');
+        //console.log(response.data.message, '\n', response.data.data);
+        console.log(response.data.message, '\n', "Auth Tokens Received");
 
-    fs.writeFile('config.js', configData, (err) => {
-      if (err) throw err;
-      console.log('Configuration file created!');
-    });
-  })
-  .catch(function (error) {
-    console.log(error);
+        const { jwtToken, refreshToken, feedToken } = response.data.data;
+
+        const configData = `module.exports = {
+  jwtToken: '${jwtToken}',
+  refreshToken: '${refreshToken}',
+  feedToken: '${feedToken}',
+  apiKey: '${apiKey}',
+  clientCode: '${clientCode}'
+};`;
+
+        fs.writeFile('config.js', configData, (err) => {
+          if (err) throw err;
+          console.log('Configuration file created!');
+        });
+      } else {
+        console.log(response.data.message, '\n', response.data.errorcode);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    // Close the readline interface
+    rl.close();
   });
-  // Close the readline interface
-  rl.close();
 });
-
